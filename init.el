c4ed4aeaef0d974adddd619ace346ac84f275c30
Christoph Uhlich <c.uhlich@tarent.de>
2020-10-02 08:45:15 +0200

    Remove pacakge initialize.

--- init.el
+++ init.el
@@ -1,4 +1,5 @@
+(setq package-enable-at-startup nil)
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
-(package-initialize)
+;(package-initialize)
 (org-babel-load-file "~/.emacs.d/config.org")
