5327ad2b187cc96fabbb8c60732f31d0ed0a57b3
Christoph Uhlich <christoph@familie-uhlich.de>
2023-02-23 08:38:52 +0100

    Allow org export template. Disable drill.

--- init.el
+++ init.el
@@ -18,3 +18,18 @@
 (use-package command-log-mode)
 
 (org-babel-load-file "~/.emacs.d/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-safe-remote-resources
+   '("\\`https://fniessen\\.github\\.io/org-html-themes/setup/theme-readtheorg\\.setup\\'"))
+ '(package-selected-packages
+   '(yasnippet which-key web-mode vue-mode visual-fill-column verb use-package unicode-fonts treemacs-magit treemacs-icons-dired treemacs-all-the-icons timu-spacegrey-theme tide rjsx-mode popup platformio-mode ox-reveal ovpn-mode origami orgtbl-aggregate org-vcard org-superstar org-super-agenda org-roam-ui org-journal org-gcal org-drill npm-mode markdown-toc lsp-ui lsp-treemacs kill-ring-search idle-highlight-mode google-translate go-projectile general forge flymake-json flycheck-pos-tip flycheck-color-mode-line eyebrowse expand-region drag-stuff doom-themes doom-modeline dockerfile-mode docker default-text-scale counsel company command-log-mode ag)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
